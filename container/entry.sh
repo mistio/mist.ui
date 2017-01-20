0b6fa3dfc78ba0b7a6081bc21ab84784914be23a
commixon <commixon@gmail.com>
2017-01-20 17:35:50 +0200

    Fix ui mounting for unison

--- container/entry.sh
+++ container/entry.sh
@@ -1,4 +1,11 @@
-cd /mist.core/src/mist.io/ui
+#!/bin/sh
+
+if [ "$1" = "unison" ];then
+    rm -rf /ui
+    ln -s /mist.core/src/mist.io/ui /ui 
+fi
+
+cd /ui
 if ! git diff --quiet --exit-code staging bower.json; then
     echo "bower.json changed"
     echo "Running bower install"
