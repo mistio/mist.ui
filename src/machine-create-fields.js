885d17ec0b53ae447ac3f8b0adfaecb227f117b0
Konstantinos Tsaprailis <kostis.tsaprailis@gmail.com>
2017-02-20 18:10:44 +0200

    Adding warning message that this is still under development

--- src/machine-create-fields.js
+++ src/machine-create-fields.js
@@ -617,7 +617,7 @@ MACHINE_CREATE_FIELDS.forEach(function(p){
         type: "textarea",
         value: "",
         defaultValue: "",
-        helptext: "Provide the desired hostname you want to assign to the machine. Example: machine1.mist.io. There needs to be a DNS zone for this domain already created.",
+        helptext: "Provide the desired hostname you want to assign to the machine. Example: machine1.mist.io. There needs to be a DNS zone for this domain already created. Currently under heavy development, might not be fully functional.",
         show: true,
         required: false,
         showIf: {
