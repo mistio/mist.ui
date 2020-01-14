c31a16f62dad02efe16be1aaf5eda4950ce3f1af
Alex Giavaras <a.giavaras@gmail.com>
2020-01-14 13:34:03 +0200

    Remove type field from LXD volume fields

--- src/volume-create-fields.js
+++ src/volume-create-fields.js
@@ -278,18 +278,6 @@ VOLUME_CREATE_FIELDS.push({
 VOLUME_CREATE_FIELDS.push({
     provider: 'lxd',
     fields: [{
-        name: 'type',
-        label: 'Volume Type *',
-        type: 'text',
-        value: '',
-        defaultValue: '',
-        show: true,
-        required: true,
-        options: [{ title: 'custom', val: 'custom'},
-                  { title: 'copy', val: 'copy'},
-                  { title: 'migration', val: 'migration'}],
-        helptext: 'Specify the volume\'s type',
-    }, {
         name: 'pool_id',
         label: 'Pool Id *',
         type: 'text',
