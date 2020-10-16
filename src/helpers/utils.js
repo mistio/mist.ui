8ac4b78c5e890c0043d3ab15fdb671bf1a0d06e8
Dimitris Moraitis <dimo@mist.io>
2020-10-16 17:21:08 +0300

    Fix title, update base paths, import numeral.js as script for now

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,8 +1,8 @@
-import '../../node_modules/numeral/src/numeral.js';
+// import '../../node_modules/numeral/src/numeral.js';
 
 function ratedCost(cost, rate) {
-    var c = parseFloat(cost || 0);
-    var rc = c * rate;
+    const c = parseFloat(cost || 0);
+    const rc = c * rate;
     return c && rc ? numeral(rc).format('0,00.00a') : numeral(cost).format('0,00.00a');
 }
 
@@ -15,19 +15,19 @@ function itemUid(item, section) {
     // TODO replace with mist uuids
 
     // if item type is not defined derive it fro the current section id
-    var item_type = item && item.type ? item.type : (section && section.id.slice(0, -1));
-    var cloudId = item && section && section.id != 'machines' && item.cloud && item.cloud.id ? item.cloud.id :
+    const itemType = item && item.type ? item.type : (section && section.id.slice(0, -1));
+    const cloudId = item && section && section.id !== 'machines' && item.cloud && item.cloud.id ? item.cloud.id :
         '';
-    var itemId = item && item.id ? item.id : '';
-    if (item_type == 'incident')
+    let itemId = item && item.id ? item.id : '';
+    if (itemType === 'incident')
         itemId = item.incident_id
-    return item_type + ':' + cloudId + ':' + itemId;
+    return `${ itemType  }:${  cloudId  }:${  itemId }`;
 }
 
 function mapToArray(obj) {
-    var arr = [];
+    const arr = [];
     if (obj) {
-        for (let id in obj) {
+        for (const id in obj) {
             arr.push(obj[id]);
         }
     }
@@ -35,25 +35,25 @@ function mapToArray(obj) {
 }
 
 function _generateMap(list, field) {
-    var out = {};
-    if (field == undefined) {
-        field = 'id';
+    const out = {};
+    let _field = field;
+    if (field === undefined) {
+        _field = 'id';
     }
-    for (var i = 0; i < list.length; i++) {
-        out[list[i][field]] = list[i];
+    for (let i = 0; i < list.length; i++) {
+        out[list[i][_field]] = list[i];
     }
     // console.log('generate map', list, field, JSON.stringify(out));
     return out;
 }
 
 function intersection(a, b){
-    a = a instanceof Set ? a : new Set(a);
-    b = b instanceof Set ? b : new Set(b);
-    return new Set( [ ...a ].filter(v => b.has(v)) );
-
+    const _a = a instanceof Set ? a : new Set(a);
+    const _b = b instanceof Set ? b : new Set(b);
+    return new Set( [ ..._a ].filter(v => _b.has(v)) );
 }
 
-var CSRFToken = { value: undefined };
+const CSRFToken = { value: undefined };
 
 export { ratedCost, itemUid, mapToArray, _generateMap, intersection, CSRFToken };
 
