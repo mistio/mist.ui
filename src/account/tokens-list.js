e74a9b1162f73a859f4d51c41b84cffedc3d2ad8
Eis <monoatomic.alex@gmail.com>
2020-11-09 08:40:08 +0200

    Fix tokens error

--- src/account/tokens-list.js
+++ src/account/tokens-list.js
@@ -6,6 +6,8 @@ import '../../node_modules/@polymer/paper-listbox/paper-listbox.js';
 import './token-item.js';
 import { Polymer } from '../../node_modules/@polymer/polymer/lib/legacy/polymer-fn.js';
 import { html } from '../../node_modules/@polymer/polymer/lib/utils/html-tag.js';
+import { CSRFToken } from '../helpers/utils.js';
+// import { PaperDialogBehavior } from "@polymer/paper-dialog-behavior/paper-dialog-behavior.js"
 
 Polymer({
   _template: html`
@@ -141,7 +143,7 @@ Polymer({
             ></paper-input>
             <p>* Password is required to create a token</p>
             <div class="buttons">
-              <paper-button dialog-dismiss="">Cancel</paper-button>
+              <paper-button dialog-dismiss>Cancel</paper-button>
               <paper-button
                 id="Create"
                 autofocus=""
@@ -193,6 +195,10 @@ Polymer({
 
   is: 'tokens-list',
 
+  behaviors: [
+    // PaperDialogBehavior
+  ],
+
   properties: {
     user: {
       type: Object,
@@ -239,7 +245,7 @@ Polymer({
   createTokenSubmit() {
     const payload = this.newToken;
     this.$.createTokenAjax.headers['Content-Type'] = 'application/json';
-    this.$.createTokenAjax.headers['Csrf-Token'] = this.user.csrf_token;
+    this.$.createTokenAjax.headers['Csrf-Token'] = CSRFToken.value;
     this.$.createTokenAjax.body = payload;
     this.$.createTokenAjax.generateRequest();
   },
