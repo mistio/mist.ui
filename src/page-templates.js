9f11a3bf44c076dd30314e022d943ee3f94869b1
faysvas@yahoo.com <faysvas@yahoo.com>
2020-11-20 18:52:52 +0200

    Add logos

--- src/page-templates.js
+++ src/page-templates.js
@@ -117,7 +117,7 @@ Polymer({
                     return `<strong class="name">${  item  }</strong>`;
                 },
                 'cmp': (row1, row2) => {
-                    return row1['name'].localeCompare(row2['name'], 'en', {sensitivity: 'base'});
+                    return row1.name.localeCompare(row2.name, 'en', {sensitivity: 'base'});
                 }
             },
             'owned_by': {
