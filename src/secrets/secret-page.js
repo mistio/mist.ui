f587db68ff488c962b30807c712acb6cdfe9e577
Eis <monoatomic.alex@gmail.com>
2021-02-11 01:06:01 +0200

    Add some css

--- src/secrets/secret-page.js
+++ src/secrets/secret-page.js
@@ -7,6 +7,66 @@ import { html } from '../../node_modules/@polymer/polymer/lib/utils/html-tag.js'
 Polymer({
     _template: html`
         <style>
+            paper-material {
+                padding: 24px;
+            }
+        
+            paper-menu-button paper-button {
+                display: block;
+            }
+            paper-button.right {
+                text-align: right;
+            }
+        
+            paper-button.link,
+            paper-button.link iron-icon {
+            background-color: transparent !important;
+            color: var(--mist-blue) !important;
+            }
+    
+            .head {
+            @apply --layout-horizontal;
+            align-items: center;
+            }
+            [size='xs'] > * {
+                display: none;
+                }
+    
+            [size='xs'] mist-sidebar {
+            min-width: 100%;
+            height: auto;
+            }
+    
+            iron-icon.bottom {
+            padding-right: 8px;
+            }
+            h4.secret {
+                @apply --layout-flex;
+                text-transform: uppercase;
+                font-size: 0.9rem;
+                font-weight: 700;
+                font-color: #fff;
+            }
+        
+            h4.id {
+                display: inline-block;
+                text-transform: uppercase;
+                font-size: 0.9rem;
+                font-weight: 700;
+                margin-right: 16px;
+            }
+            #showSecretbtn iron-icon {
+                color: var(--paper-button-text) !important;
+                width: 20px;
+                height: 20px;
+            }
+            secret-actions {
+                fill: #fff;
+                min-width: 50%;
+            }        
+            .secret-info > paper-material > span {
+            margin-right: 16px;
+            }
         </style>
         <div id="content">
             <paper-material class="single-head layout horizontal">
@@ -19,18 +79,18 @@ Polymer({
                 <secret-actions></secret-actions>
             </paper-material>
             <div id="secret-info">
-                <paper-material>
+                <paper-material class="single-head layout horizontal">
                     <h4 class="id">Secret ID:</h4><span class="id">[[secret.id]]</span>
                     <h4 class="id" hidden\$="[[!secret.owned_by.length]]">Owner:</h4><span class="id">[[_displayUser(secret.owned_by,model.members)]]</span>
                     <h4 class="id" hidden\$="[[!secret.created_by.length]]">Created by:</h4><span class="id">[[_displayUser(secret.created_by,model.members)]]</span>
                 </paper-material>
                 <paper-material>
                     <div class="head">
-                        <h4 class="key">Secret</h4>
-                        <paper-button hidden\$="[[!visibleSecret]]" id="hideSecretbtn" on-tap="hideSecret" class="right link">Hide <span class="wide">Private Key</span>
+                        <h4 class="secret">Secret</h4>
+                        <paper-button hidden\$="[[!visibleSecret]]" id="hideSecretbtn" on-tap="hideSecret" class="right link">Hide <span class="wide">Secret</span>
                             <iron-icon icon="icons:visibility-off"></iron-icon>
                         </paper-button>
-                        <paper-button hidden\$="[[!visibleSecret]]" on-tap="copySecret" class="right link">Copy <span class="wide">private key</span>
+                        <paper-button hidden\$="[[!visibleSecret]]" on-tap="copySecret" class="right link">Copy <span class="wide">Secret</span>
                             <iron-icon icon="content-copy"></iron-icon>
                         </paper-button>
                     </div>
@@ -63,6 +123,9 @@ Polymer({
         secretValue:{
             type: Object,
             value: {}
+        },
+        model:{
+            type: Object
         }
     },
 
