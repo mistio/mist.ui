ce95f7c67b863bb4cfb39a08b1ec5b6f3e14d356
Dimitris Moraitis <dimo@mist.io>
2021-04-11 04:18:26 +0300

    Update secret icon

--- src/mist-app.js
+++ src/mist-app.js
@@ -1116,7 +1116,7 @@ Polymer({
       {
         id: 'secrets',
         color: '#1D1C1A',
-        icon: 'device:screen-lock-portrait',
+        icon: 'icons:lock',
         add: '/secrets/+add',
         sidebar: true,
         tile: true,
