a6e31cc55e76a4b9ee6de07177ed716876bb8f07
faysvas@yahoo.com <faysvas@yahoo.com>
2021-09-10 21:53:42 +0300

    Add volume fields from api v2

--- src/machines/machine-create-helpers/machine-create-form-data.js
+++ src/machines/machine-create-helpers/machine-create-form-data.js
@@ -1,13 +1,27 @@
 import { MACHINE_NAME_REGEX_PATTERNS } from './machine-name-regex-patterns.js';
 
-const MACHINE_CREATE_FORM_DATA = {
+const MACHINE_CREATE_FORM_DATA = data => ({
   src: './assets/forms/create-machine.json',
   formData: {
     dynamicData: {
         clouds: {
           func: new Promise(resolve => {
             // Wait until clouds have loaded here
-            resolve(() => this.clouds)
+            resolve(() => data.clouds)
+          }),
+        },
+        getZones: {
+          func: new Promise(resolve => {
+            // Wait until clouds have loaded here
+            resolve(
+              () => data._toArray(data.model.zones).map(zone=>{
+                return {
+                  id: zone.id,
+                  title: zone.zone_id
+                }
+              })
+
+            )
           }),
         }
       },
@@ -17,7 +31,7 @@ const MACHINE_CREATE_FORM_DATA = {
         },
         getNameRegex: {
           func: cloudId => {
-            const provider =  this._getCloudById(cloudId) && this._getCloudById(cloudId).provider;
+            const provider =  data._getCloudById(cloudId) && data._getCloudById(cloudId).provider;
             const pattern = (provider && MACHINE_NAME_REGEX_PATTERNS[provider]) || MACHINE_NAME_REGEX_PATTERNS['default'];
         console.log("pattern ", pattern);
         return pattern;
@@ -29,7 +43,7 @@ const MACHINE_CREATE_FORM_DATA = {
         getLocationsFromCloud: {
           func: cloudId => {
             if (!cloudId) { return;}
-            const locationsArray = this._getCloudById(cloudId).locationsArray || [];
+            const locationsArray = data._getCloudById(cloudId).locationsArray || [];
             const locations =  locationsArray.map(
               location => (
                 {...location, title:location.name}
@@ -42,7 +56,7 @@ const MACHINE_CREATE_FORM_DATA = {
         getImagesFromCloud: {
           func: cloudId => {
             if (!cloudId) { return;}
-            const imagesArray = this._getCloudById(cloudId).imagesArray || [];
+            const imagesArray = data._getCloudById(cloudId).imagesArray || [];
             const images =  imagesArray.map(
               image => (
                 {...image, title:image.name}
@@ -60,15 +74,71 @@ const MACHINE_CREATE_FORM_DATA = {
         getSizesFromCloud: {
           func: cloudId => {
             if (!cloudId) { return;}
-            const cloudSize = this._getCloud(cloudId) || {};
+            const cloudSize = data._getCloud(cloudId) || {};
             return cloudSize.size;
           }
         },
         getSizesFromLocation: {
           func: cloudId => { return ["a", "b", "c"]},
         },
+        showVolumeContainer: {
+          func: cloudId => {
+            const provider = data._getProviderById(cloudId);
+            const cloudsWithVolumes = ['ec2', 'azure','digitalocean', 'equinixmetal','gce','linode','openstack']
+            return !cloudsWithVolumes.includes(provider);
+          },
+        },
+        showDeviceNameInNewVolume: {
+          func: cloudId => ['equinixmetal', 'openstack'].includes(data._getProviderById(cloudId))
+        },
+        showVolumeBoot: {
+          func: cloudId => !['gce', 'openstack'].includes(data._getProviderById(cloudId))
+        },
+        showVDeleteTerminationInNewVolume: {
+          func: cloudId => !['ec2', 'openstack'].includes(data._getProviderById(cloudId))
+        },
+        showIfAmazon: {
+          func: cloudId => !data._getProviderById(cloudId) === 'ec2'
+        },
+        showIfAzure: {
+          func: cloudId => !data._getProviderById(cloudId) === 'azure'
+        },
+        showIfDigitalOcean: {
+          func: cloudId => !data._getProviderById(cloudId) === 'digitalocean'
+        },
+        showIfEquinix: {
+          func: cloudId => !data._getProviderById(cloudId) === 'equinixmetal'
+        },
+        // One volume should set this
+        showIfGoogle: {
+          func: cloudId => !data._getProviderById(cloudId) === 'gce'
+        },
+        showIfLinode: {
+          func: cloudId => !data._getProviderById(cloudId) === 'linode'
+        },
+        showIfOpenstack: {
+          func: cloudId => !data._getProviderById(cloudId) === 'openstack'
+        },
+        showExistingVolume: {
+          func: choice => { console.log("choice ", choice); return choice === 'Create new volume'}
+        },
+        getVolumesFromLocations: {
+          func: location => {
+            const volumes = [];
+            for (const [key, value] of Object.entries(data.volumes)) {
+              if(value.location === location) {
+                volumes.push({
+                  title: value.name,
+                  id: value.id
+                })
+              }
+            }
+            return volumes;
+
+          }
+        }
       }
   },
-};
+});
 
 export { MACHINE_CREATE_FORM_DATA };
