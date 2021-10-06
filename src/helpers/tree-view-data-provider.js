dd028c73214112a94e8fa5de3f774265e9df8b97
Eis <monoatomic.alex@gmail.com>
2021-10-06 15:54:03 +0300

    Remove tree-view-data-provider wrapper, change property to dataProvider, bind this to tree-view-data-provider in page-machines

--- src/helpers/tree-view-data-provider.js
+++ src/helpers/tree-view-data-provider.js
@@ -1,43 +1,50 @@
-/* eslint-disable no-param-reassign */
-export default function treeViewDataProvider(grid) {
-  return (opts, callback) => {
-    let items = (Array.isArray(grid.items) ? grid.items : []).slice(0);
-    if (grid._filters && grid._checkPaths(grid._filters, 'filtering', items))
-      items = grid._filter(items);
+/* eslint-disable no-param-reassign, consistent-return */
+export default function treeViewDataProvider(opts, callback) {
+  let items = [];
+  const mistList = this.shadowRoot.querySelector('mist-list');
+  if (!mistList) return [];
+  const { grid } = mistList.$;
+  if (!grid) return [];
+  items = (Array.isArray(grid.items) ? grid.items : []).slice(0);
+  if (grid._filters && grid._checkPaths(grid._filters, 'filtering', items))
+    items = grid._filter(items);
 
-    grid.size = items.length;
-    if (
-      opts.sortOrders.length &&
-      grid._checkPaths(grid._sorters, 'sorting', items)
-    )
-      items = items.sort(grid._multiSort.bind(grid));
+  grid.size = items.length;
+  if (
+    opts.sortOrders.length &&
+    grid._checkPaths(grid._sorters, 'sorting', items)
+  )
+    items = items.sort(grid._multiSort.bind(grid));
 
-    if (this.filteredItems && this.filter && this.filter.trim().length > 0) {
-      const filterMap = {};
-      this.filteredItems.forEach(item => {
-        filterMap[item.id] = item;
-      });
-      // add parents
-      Object.values(filterMap).forEach(item => {
-        if (item.parent) filterMap[item.parent] = this.itemMap[item.parent];
-      });
-      items = Object.values(filterMap);
-    }
-    let data = [];
-    if (!this.treeView) {
-      data = items;
-      this.count = data.length;
-    } else if (opts.parentItem) {
-      data = items.filter(item => item.parent === opts.parentItem.id);
-      this.count += data.length;
-    } else {
-      data = items.filter(item => !item.parent);
-      this.count = data.length;
-    }
-    const start = opts.page * opts.pageSize;
-    const end = start + opts.pageSize;
-    const slice = data.slice(start, end);
-    callback(slice, data.length);
-  };
+  if (
+    mistList.filteredItems &&
+    mistList.combinedFilter &&
+    mistList.combinedFilter.trim().length > 0
+  ) {
+    const filterMap = {};
+    mistList.filteredItems.forEach(item => {
+      filterMap[item.id] = item;
+    });
+    // add parents
+    Object.values(filterMap).forEach(item => {
+      if (item.parent) filterMap[item.parent] = mistList.itemMap[item.parent];
+    });
+    items = Object.values(filterMap);
+  }
+  let data = [];
+  if (!mistList.treeView) {
+    data = items;
+    mistList.count = data.length;
+  } else if (opts.parentItem) {
+    data = items.filter(item => item.parent === opts.parentItem.id);
+    mistList.count += data.length;
+  } else {
+    data = items.filter(item => !item.parent);
+    mistList.count = data.length;
+  }
+  const start = opts.page * opts.pageSize;
+  const end = start + opts.pageSize;
+  const slice = data.slice(start, end);
+  callback(slice, slice.length);
 }
-/* eslint-enable no-param-reassign */
+/* eslint-enable no-param-reassign, consistent-return */
