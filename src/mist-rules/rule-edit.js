fe7b8f609fda5d1c178546b90cd3b3aa0bc48c2c
Eis <monoatomic.alex@gmail.com>
2021-10-13 20:36:46 +0300

    Separate monitoring from metering metrics, show new names for victoria

--- src/mist-rules/rule-edit.js
+++ src/mist-rules/rule-edit.js
@@ -1144,10 +1144,10 @@ Polymer({
       : '';
   },
   _handleMetricResponse(data) {
-    const output = {};
     if (data.detail.response) {
       // console.log('_handleMetricResponse response data', data.detail.response);
-      const allMetrics = {};
+      const meteringMetrics = {};
+      const monitoringMetrics = {};
       // By doing the below two metrics with similar name eg.
       // mem.active and mem_active (influx & victoria)
       // will result in a single metric in the output, the one that gets
@@ -1157,53 +1157,48 @@ Polymer({
         if (
           data.detail.response[metricName].method === 'telegraf-victoriametrics'
         ) {
-          let metric = metricName.replace('}', '');
+          let metric = metricName.split('{')[0].replace('}', '');
           metric = metric.replace('{', '_');
           metric = metric.replaceAll('_', '.');
-          allMetrics[metric] = data.detail.response[metricName];
+          if (metricName.indexOf('metering') > -1)
+            meteringMetrics[metric] = data.detail.response[metricName];
+          else monitoringMetrics[metric] = data.detail.response[metricName];
         } else {
-          allMetrics[metricName] = data.detail.response[metricName];
+          monitoringMetrics[metricName] = data.detail.response[metricName];
         }
       });
-      Object.keys(allMetrics).forEach(metric => {
-        let res = output;
-        const chunks = metric.split('.');
-        try {
-          for (let i = 0; i < chunks.length; i++) {
-            if (!res[chunks[i]]) {
-              res[chunks[i]] = {};
-            }
-            if (i === chunks.length - 1) {
-              res[chunks[i]] = allMetrics[metric].id;
-            }
-            res = res[chunks[i]];
-          }
-        } catch (e) {
-          console.warn(
-            `Metric ${metric} encountered a problem during listing.`
-          );
-          output[chunks[0]][metric] = allMetrics[metric].id;
-        }
-      });
-    }
-    this.set('availableMetrics', this._computeMetricsArray(output));
-    // Store metrics in resource if available, ie we are in a single page, so as to improve performance
-    if (!this.model.metrics) {
-      this.model.metrics = {};
-    }
-    this.model.metrics[this.metricsUri] = this.availableMetrics;
-    // handle empty response
-    if (!output) {
-      this.dispatchEvent(
-        new CustomEvent('toast', {
-          bubbles: true,
-          composed: true,
-          detail: {
-            msg: 'No metrics available for this resource(s)',
-            duration: 3000,
-          },
-        })
-      );
+      const outputMetering = this._getMetricsLayering(meteringMetrics);
+      const outputMonitoring = this._getMetricsLayering(monitoringMetrics);
+      const metricsArray = [
+        {
+          name: 'monitoring',
+          options: this._computeMetricsArray(outputMonitoring),
+        },
+      ];
+      if (Object.keys(outputMetering).length > 0)
+        metricsArray.push({
+          name: 'metering',
+          options: this._computeMetricsArray(outputMetering),
+        });
+      this.set('availableMetrics', metricsArray);
+      // Store metrics in resource if available, ie we are in a single page, so as to improve performance
+      if (!this.model.metrics) {
+        this.model.metrics = {};
+      }
+      this.model.metrics[this.metricsUri] = this.availableMetrics;
+      // handle empty response
+      if (!outputMonitoring || !outputMetering) {
+        this.dispatchEvent(
+          new CustomEvent('toast', {
+            bubbles: true,
+            composed: true,
+            detail: {
+              msg: 'No metrics available for this resource(s)',
+              duration: 3000,
+            },
+          })
+        );
+      }
     }
   },
   _computeMetricsArray(output) {
@@ -1233,6 +1228,29 @@ Polymer({
         elem.options.splice(0, 0, elem.options.splice(totalIndex, 1)[0]);
     });
     return arr.sort((a, b) => a.name.localeCompare(b.name));
+  },
+
+  _getMetricsLayering(metrics) {
+    const output = {};
+    Object.keys(metrics).forEach(metric => {
+      let res = output;
+      const chunks = metric.split('.');
+      try {
+        for (let i = 0; i < chunks.length; i++) {
+          if (!res[chunks[i]]) {
+            res[chunks[i]] = {};
+          }
+          if (i === chunks.length - 1) {
+            res[chunks[i]] = metrics[metric].id;
+          }
+          res = res[chunks[i]];
+        }
+      } catch (e) {
+        console.warn(`Metric ${metric} encountered a problem during listing.`);
+        output[chunks[0]][metric] = metrics[metric].id;
+      }
+    });
+    return output;
   },
 
   _handleMetricError(error) {
