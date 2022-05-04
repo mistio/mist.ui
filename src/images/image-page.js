a24e4f14ab169625865c2a610185d01d63245900
Eis <monoatomic.alex@gmail.com>
2022-05-04 11:05:18 +0300

    Remove model from image-page

--- src/images/image-page.js
+++ src/images/image-page.js
@@ -310,9 +310,6 @@ Polymer({
     params: {
       type: Object,
     },
-    model: {
-      type: Object,
-    },
     image: {
       type: Object,
       notify: true,
@@ -335,14 +332,6 @@ Polymer({
   observers: ['_changed(image)'],
 
   ready() {},
-
-  // _displayUser(id, _members) {
-  //   return this.model && id && this.model.members && this.model.members[id]
-  //     ? this.model.members[id].name ||
-  //         this.model.members[id].email ||
-  //         this.model.members[id].username
-  //     : '';
-  // },
 
   _changed(image) {
     if (image) this.set('itemArray', [this.image]);
