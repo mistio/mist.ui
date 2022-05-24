74062c4a99efe81d1e17941b4c6a36addfab93f3
Eis <monoatomic.alex@gmail.com>
2022-05-24 14:16:40 +0300

    Rearrange dialog items, remove scrollbar, introduce default values for minimum and maximum nodes

--- src/clusters/edit-nodepool.js
+++ src/clusters/edit-nodepool.js
@@ -81,12 +81,26 @@ export default class EditNodepool extends PolymerElement {
           <div id="content">
             <h2>Edit Nodepool</h2>
             <br />
-            <div class="layout horizontal">
+            <div>
+              <template
+                is="dom-if"
+                if="[[_showDesiredNodes(payload.autoscaling)]]"
+                restamp=""
+              >
+                <paper-input
+                  id="desired_nodes"
+                  label="Number of nodes"
+                  on-change="_valueChanged"
+                  value="[[nodepool.node_count]]"
+                >
+                </paper-input>
+              </template>
               <template
                 is="dom-if"
                 if="[[_showAutoscalingToggle()]]"
                 restamp=""
               >
+                <div class="layout horizontal">
                 <paper-toggle-button
                   id="autoscale"
                   checked="[[payload.autoscaling]]"
@@ -94,6 +108,7 @@ export default class EditNodepool extends PolymerElement {
                 >
                 </paper-toggle-button>
                 <span> Enable Autoscaling </span>
+                </div>
               </template>
             </div>
             <template
@@ -116,19 +131,7 @@ export default class EditNodepool extends PolymerElement {
               >
               </paper-input>
             </template>
-            <template
-              is="dom-if"
-              if="[[_showDesiredNodes(payload.autoscaling)]]"
-              restamp=""
-            >
-              <paper-input
-                id="desired_nodes"
-                label="Number of nodes"
-                on-change="_valueChanged"
-                value="[[nodepool.node_count]]"
-              >
-              </paper-input>
-            </template>
+            <br>
             <paper-button disabled="[[formError]]" on-tap="submit"
               >Submit</paper-button
             >
@@ -162,8 +165,8 @@ export default class EditNodepool extends PolymerElement {
 
   _clearSelection() {
     if (this.nodepool) {
-      this.payload.min_nodes = this.nodepool.min_nodes;
-      this.payload.max_nodes = this.nodepool.max_nodes;
+      this.payload.min_nodes = this.nodepool.min_nodes || this.nodepool.node_count;
+      this.payload.max_nodes = this.nodepool.max_nodes || this.nodepool.node_count;
       this.payload.desired_nodes = this.nodepool.node_count;
       this.set('payload.autoscaling', this.nodepool.autoscaling);
     }
@@ -183,7 +186,7 @@ export default class EditNodepool extends PolymerElement {
       (!(this.payload.min_nodes <= this.payload.desired_nodes) ||
         !(this.payload.max_nodes >= this.payload.desired_nodes))
     ) {
-      msg = 'EKS requires Min Nodes <= Desired Nodes <= Max Nodes';
+      msg = 'EKS requires Minimum Nodes <= Desired Nodes <= Maximum Nodes';
     } else if (
       this.payload.autoscaling &&
       (!this.payload.min_nodes || !this.payload.max_nodes)
